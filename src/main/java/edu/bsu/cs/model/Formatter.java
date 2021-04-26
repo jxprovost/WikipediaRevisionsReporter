24841d461a4419a5a5d7c056e5057f546f305a78
Jaren Provost <jxprovost@bsu.edu>
2021-04-26 14:32:20 -0400

    Quick change to code.

--- src/main/java/edu/bsu/cs/model/Formatter.java
+++ src/main/java/edu/bsu/cs/model/Formatter.java
@@ -3,6 +3,5 @@ package edu.bsu.cs.model;
 import java.io.IOException;
 
 public interface Formatter {
-    String format(Revision revision) throws Exception;
     String alternativeFormat(Revision revision) throws IOException;
 }
